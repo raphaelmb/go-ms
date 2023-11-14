a80530329bf33cdf421df4e507c8013eb2e3e6fa
Raphael Berger <raphaelmb@gmail.com>
2023-11-14 14:40:04 -0300

    add broker service + more

---
+++ broker-service/cmd/api/handlers.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
+	payload := jsonResponse{
+		Error:   false,
+		Message: "working",
+	}
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const port = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+	log.Printf("Starting broker service on port %s\n", port)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: app.routes(),
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
